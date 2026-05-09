ab0396026409ab1fa1121df514a451844334a6d1
DESKTOP-TMPISVG\Mircea <avram.mircea.x4h@student.ucv.ro>
2026-05-10 00:42:12 +0300

    mmm

---
+++ ClinicaMedicala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClinicaMedicala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
