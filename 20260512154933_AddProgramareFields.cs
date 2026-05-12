795f9847e83fbdf9b06e9f4394235f00a774c6db
Fota Ioan-Valeriu <170747702+FotaValeriu@users.noreply.github.com>
2026-05-12 19:04:35 +0300

    Add files via upload
    finalizare branch

---
+++ 20260512154933_AddProgramareFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicaMedicala.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProgramareFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
